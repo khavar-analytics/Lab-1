8de2b8d33f51c78c62bdf314d2d1c7ce1eba4820
khavar-analytics <gasimovakhaver@gmail.com>
2026-02-18 20:54:26 +0400

    Add Jupyter notebook and Python script

---
+++ m1-01-task-3-functions.py
@@ -0,0 +1,18 @@
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def cleaned_version(value):
+    if not isinstance(value, str):
+        return None
+    return value.strip().lower()
+
+
+checking_lst = [33, " dyvAJSj", "hedgd", "23.56", 45, "igde", "555.76"]
+for i in checking_lst:
+    print(is_float(i))
+    print(cleaned_version(i))
